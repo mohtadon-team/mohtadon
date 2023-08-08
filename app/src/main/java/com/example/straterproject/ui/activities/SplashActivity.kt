35039636a3d6dbe15578df0b51e8d045eab74dd7
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2023-08-08 19:17:37 +0300

    add home prayer times screen

--- app/src/main/java/com/example/straterproject/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/example/straterproject/ui/activities/SplashActivity.kt
@@ -1,15 +1,10 @@
 package com.example.straterproject.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.example.data.dataSource.remote.service.PrayerTimesService
 import com.example.straterproject.R
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,22 +13,11 @@ class SplashActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
 
-//lifecycleScope.launch {
-//    Retrofit.Builder().baseUrl("http://api.aladhan.com")
-//        .client(OkHttpClient.Builder().build())
-//        .addConverterFactory(GsonConverterFactory.create()).build()
-//        .create(PrayerTimesService::class.java).getTodayPrayerTimes()
-//        .body()?.data?.timings?.let {
-//            Log.i(
-//                "ahmed", it.Asr
-//            )
-//        }
-//}
 
-//        Handler().postDelayed({
-//            val intent= Intent(this, HomeActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        },3000)
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
