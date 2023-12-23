5c3e503313475c4780a1f9df27453024b7cde2f6
3slam <3slammohsen@gmail.com>
2023-12-23 09:00:38 -0800

    after completing the UI. but there are some bugs need to be solve.

---
+++ app/src/main/java/com/example/straterproject/ui/reciters/reciterInfo/ReciterUiSate.kt
@@ -0,0 +1,15 @@
+package com.example.straterproject.ui.reciters.reciterInfo
+
+
+import com.example.domain.entity.reciters.ReciterEntity
+
+data class ReciterUiSate(
+    var isTabTitleVisible :Boolean = true,
+    var isTabSearchVisible :Boolean = false,
+    var searchText : String = "",
+    var reciters : List<ReciterEntity> = emptyList(),
+    var isLoading : Boolean  = false,
+    var isError :Boolean = false,
+    var errorMessage :String = "",
+    var isDataExist  :Boolean = false
+)
