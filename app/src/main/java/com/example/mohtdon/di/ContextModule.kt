63dce5253f11b415adb09a3695bd29cf82355162
abaferas <wasfyelgamed@gmail.com>
2024-01-17 00:12:10 +0200

    implement context module hilt dependency

---
+++ app/src/main/java/com/example/mohtdon/di/ContextModule.kt
@@ -0,0 +1,20 @@
+package com.example.mohtdon.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ContextModule {
+
+    @Provides
+    @Singleton
+    fun provideApplicationContext(@ApplicationContext context: Context): Context {
+        return context
+    }
+}
