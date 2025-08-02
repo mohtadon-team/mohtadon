2e83567156d3c72498870868356a647140d7c73e
Hassan Wasfy <wasfyelgamed@gmail.com>
2025-08-02 22:55:38 +0300

    fix sebha reset error.

--- app/src/main/java/com/mohtdon/ui/sebha/fragments/RosaryFragment.kt
+++ app/src/main/java/com/mohtdon/ui/sebha/fragments/RosaryFragment.kt
@@ -1,7 +1,11 @@
 package com.mohtdon.ui.sebha.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.ViewModel
 import com.mohtdon.mohtdon.R
 import com.mohtdon.mohtdon.databinding.FragmentRosaryBinding
@@ -21,8 +25,14 @@ class RosaryFragment : BaseFragment<FragmentRosaryBinding>() {
         binding.counterTv.setOnClickListener {
             counter++
             binding.counterTv.text = counter.toString()
+            if (binding.counterTxt.hasFocus()) {
+                binding.counterTxt.clearFocus()
+                val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(binding.counterTxt.windowToken, 0)
+            }
         }
         binding.resetBtn.setOnClickListener {
+            counter = 0
             binding.counterTv.text="0"
         }
     }
